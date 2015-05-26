c93eb1c06fa3c675927aed4f3c2e540e3acfaad9
Administrator <Administrator@MSDN-SPECIAL>
2015-05-26 11:47:42 +0900

    5.26

--- jse/src/oop06/tryCatch/TryCatchDemo.java
+++ jse/src/oop06/tryCatch/TryCatchDemo.java
@@ -6,6 +6,14 @@ package oop06.tryCatch;
  예외는 코드상에 발생한 에러와 다르며 컴파일시 에러가
  발생하지 않음.
  */
+/*
+ try{
+ 	예외가 발생할 소지가 있는 코드를 개발자가 선별하여 지정.
+ 	IO, DBMS, Network 관련 코드가 주로 위치.
+ }catch(Exception ex){
+ 	ex.printStackTrace();
+ }
+ */
 public class TryCatchDemo {
 	public static void main(String[] args) {
 		int a=5, b=0;
